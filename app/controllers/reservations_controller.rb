236bac17b6402f1f9a1355bd8df208fb77cd2954
Marcela Fernandez <marferpu17@gmail.com>
2022-09-29 18:56:48 -0500

    Actions controller: index, new, create

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,11 +1,21 @@
 class ReservationsController < ApplicationController
   def index
+    @reservations = Reservation.all
   end
 
   def new
+    @reservation = Reservation.new
   end
 
   def create
+    @place = Place.find(params[:reservation_id])
+    @reservation = Reservation.new(reservation_params)
+    @reservation.place = @place
+    if @reservation.save
+      redirect_to place_path(@place)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
