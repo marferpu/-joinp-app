8b88937e5eedc00a1313d33b5dfc24fa82d21311
Marcela Fernandez <marferpu17@gmail.com>
2022-09-29 19:41:06 -0500

    Actions controller: edit, update, destroy + modificaciones del new, create

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,8 @@
 class ReservationsController < ApplicationController
+  before_action :set_reservations, only: %i[new create show edit update destroy]
   def index
-    @reservations = Reservation.all
+    # @place = Place.find(params[:place_id])
+    @reservations = Reservation.all # se debe conectar con el place?
   end
 
   def new
@@ -8,7 +10,6 @@ class ReservationsController < ApplicationController
   end
 
   def create
-    @place = Place.find(params[:reservation_id])
     @reservation = Reservation.new(reservation_params)
     @reservation.place = @place
     if @reservation.save
@@ -22,16 +23,32 @@ class ReservationsController < ApplicationController
   end
 
   def update
+    @reservation = Reservation.update(params[reservation_params])
+    @reservation.place = @place
+    if @reservation.save
+      redirect_to place_path(@place)
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
-  def show
-  end
+  # def show
+  #   @reservation = Reservation.find(params[:id])
+  # end
 
   def destroy
+    @reservation = Reservation.find(params[:id])
+    @reservation.destroy
+    redirect_to place_path(@reservation.place), status: :see_other
   end
 
   private
 
   def reservation_params
+    params.require(:reservations).permit(:date, :visit_hour, :hour_quantity)
+  end
+
+  def set_place
+    @place = Place.find(params[:place_id])
   end
 end
