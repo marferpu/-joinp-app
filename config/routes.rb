dd827f585927a3e699ab5e94e17661db83b07190
Marcela Fernandez <marferpu17@gmail.com>
2022-10-01 13:51:40 -0500

    se organiza un poco el front

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,9 @@
 Rails.application.routes.draw do
+  root to: "places#index"
+  devise_for :users
+
   resources :places do
     resources :reservations, only: [:new, :create]
   end
   resources :reservations, only: [:show, :edit, :update, :destroy]
-  # get 'places'
-  # get 'places/new'
-  # post 'places'
-  # get 'places/:id/edit'
-  # patch 'places/:id'
-  # get 'places/:id'
-  # delete 'places/:id'
-  # get 'users/:id', to: 'users#show'
-  devise_for :users
-  root to: "places#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
