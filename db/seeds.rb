407c5e2f0fa3bac422a09d1c4845b6b7122f17e4
Marcela Fernandez <marferpu17@gmail.com>
2022-10-01 12:06:13 -0500

    se organiza seeds y algo de front

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-user = User.new(first_name: "Sebas", last_name: "H", email: "sh-9@email.com", password: "123456")
+puts "Cleaning database..."
+User.destroy_all
+Place.destroy_all
+user = User.new(first_name: "Pepe", last_name: "H", email: "pp@email.com", password: "123456")
 user.save!
-user1 = User.new(first_name: "Marce", last_name: "F", email: "mf@email.com")
+user1 = User.new(first_name: "Marce", last_name: "F", email: "mfp@email.com", password: "123456")
+user1.save!
 place1 = Place.new(title: "Place 1", description: "Lugar bonito", address: "Medellín", price_per_hour: 10000, capicity: 3, photo_url: "https://www.google.com/aclk?sa=l&ai=DChcSEwiG2rqlsrv6AhVIIUwKHQa_D8wYABAFGgJvYQ&sig=AOD64_2PUMAakU73msCZ-jWBUl7lCssQJQ&adurl&ctype=5&ved=2ahUKEwju2Kqlsrv6AhWRjuAKHSFFAXEQvhd6BAgBEHY", category: "casa", user_id: user1.id)
-place2 = Place.new(title: "Place 2", description: "Lugar bonito para compartir", address: "Medellín", price_per_hour: 40000, capicity: 8, photo_url: "https://www.google.com/aclk?sa=l&ai=DChcSEwiG2rqlsrv6AhVIIUwKHQa_D8wYABAFGgJvYQ&sig=AOD64_2PUMAakU73msCZ-jWBUl7lCssQJQ&adurl&ctype=5&ved=2ahUKEwju2Kqlsrv6AhWRjuAKHSFFAXEQvhd6BAgBEHY", category: "parqueadero", user_id: user.id  )
+place1.save!
+place2 = Place.new(title: "Place 3", description: "Lugar para compartir", address: "Medellín", price_per_hour: 40000, capicity: 8, photo_url: "https://www.google.com/aclk?sa=l&ai=DChcSEwiG2rqlsrv6AhVIIUwKHQa_D8wYABAFGgJvYQ&sig=AOD64_2PUMAakU73msCZ-jWBUl7lCssQJQ&adurl&ctype=5&ved=2ahUKEwju2Kqlsrv6AhWRjuAKHSFFAXEQvhd6BAgBEHY", category: "parqueadero", user_id: user.id  )
 place2.save!
 
 puts "Cargando datos Users"
