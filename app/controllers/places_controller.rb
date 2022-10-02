92c73d295ea7a3e7f22cd94dbf1c03812dee6170
carlos espinal <andrescar666@gmail.com>
2022-10-02 17:16:48 -0500

    Avance

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,8 +1,10 @@
 class PlacesController < ApplicationController
-  before_action :set_place, only: [:show, :edit, :update, :destroy]
+  before_action :set_place, only: %i[show edit update destroy]
   def index
     @places = Place.all
   end
+
+  def show; end
 
   def new
     @place = Place.new
@@ -10,36 +12,33 @@ class PlacesController < ApplicationController
 
   def create
     @place = Place.new(place_params)
-    @place.user = current_user
+    # @place.user = current_user
     if @place.save
-      redirect_to place_path(@place)
+      redirect_to place_path, notice: "Se ha agregado correctamente"
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
   def edit; end
 
   def update
-    @place.update(place_params)
-    if @place.save
-      redirect_to place_path(@place)
+    if @place.update(place_params)
+      redirect_to place_path, notice: "Actualización exitosa"
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  def show; end
-
   def destroy
     @place.destroy
-    redirect_to place_path(@place)
+    redirect_to place_path, notice: "Eliminación exitosa", status: :see_other
   end
 
   private
 
   def place_params
-    params.require(:place).permit(:title, :description, :address, :price_per_hour, :capicity, :category, photos: [])
+    params.require(:place).permit(:title, :description, :address, :price_per_hour, :capicity, :category, photo: [])
   end
 
   def set_place
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,11 @@
 class Place < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :address, presence: true
+  validates :price_per_hour, presence: true
+  validates :capicity, presence: true
+  validates :category, presence: true
   belongs_to :user
   has_many :reservations
-  has_many_attached :photos
+  has_one_attached :photo
 end
